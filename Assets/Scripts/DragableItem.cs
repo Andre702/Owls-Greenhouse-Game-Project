45a2b3202b438b9c90aeacc8860ffb42de4b8efd
Filip Andrzejewski <Andre.gameplay@o2.pl>
2024-03-02 15:32:50 +0200

    Draging seeds around functional
    Can drag a seed across the screen with smooth movement.
    Seed returns to original parent when released

--- Assets/Scripts/DragableItem.cs
+++ Assets/Scripts/DragableItem.cs
@@ -5,19 +5,27 @@ using UnityEngine.EventSystems;
 
 public class DragableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public Vector3 screen_position;
+    Transform originalSlot;
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin Drag");
+        originalSlot = transform.parent;
+        transform.SetParent(transform.parent.parent);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("Draging");
+        Debug.Log(Input.mousePosition);
+        screen_position = Input.mousePosition;
+        screen_position.z = Camera.main.nearClipPlane + 8;
+        transform.position = Camera.main.ScreenToWorldPoint(screen_position);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("End Drag");
+        transform.SetParent(originalSlot);
     }
 
 }
